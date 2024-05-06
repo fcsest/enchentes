ed8e02631373273b7eb08f5cb053d12ea4838229
angelorbastos <angelo.r.bastos@gmail.com>
2024-05-06 23:49:39 +0000

    Layers das cheias do IPH

--- index.R
+++ index.R
@@ -4,25 +4,69 @@ devtools::install_github("rstudio/leaflet")
 
 install.packages('sf')
 install.packages('here')
+install.packages('purrr')
 
 # Load packages
 library(sf)
 library(here)
+library(purrr)
 library(leaflet)
 library(crosstalk)
 
-# Load data
-cheias_iph_filename <- here("data", "cheias-iph.kmz")
-cheias_iph_layers <- st_layers(cheias_iph_filename)[[1]]
-
-cheias_iph <- sf::st_read(
-  here("data", "cheias-iph.kmz"),
-  layer = cheias_iph_layers[1]
-)
-
 # Render UI
-bscols(
-    leaflet(cheias_iph[1]) %>%
-        addTiles()
-)
-
+leaflet() %>%
+  addTiles() %>%
+  addPolygons(
+    data = sf::st_read(
+      here("data", "cheias-iph.kmz"),
+      layer = "Inundação com nível 550 cm (5.5m)"
+    ) %>% sf::st_zm()
+  ) %>%
+  addPolygons(
+    data = sf::st_read(
+      here("data", "cheias-iph.kmz"),
+      layer = "Inundação com nível 500 cm (5.0 m)"
+    ) %>% sf::st_zm()
+  ) %>%
+  addPolygons(
+    data = sf::st_read(
+      here("data", "cheias-iph.kmz"),
+      layer = "Cheia de 1941 (476 cm)"
+    ) %>% sf::st_zm()
+  ) %>%
+  addMarkers(
+    data = sf::st_read(
+      here("data", "cheias-iph.kmz"),
+      layer = "Sistema de Proteção - Comportas e CBs"
+    ) %>% sf::st_zm()
+  ) %>%
+  addPolylines(
+    data = sf::st_read(
+      here("data", "cheias-iph.kmz"),
+      layer = "Sistema de Proteção - Muro e Diques"
+    ) %>% sf::st_zm()
+  ) %>%
+  addMarkers(
+    data = sf::st_read(
+      here("data", "cheias-iph.kmz"),
+      layer = "DMAE - ETAs"
+    ) %>% sf::st_zm()
+  ) %>%
+  addPolygons(
+    data = sf::st_read(
+      here("data", "cheias-iph.kmz"),
+      layer = "DMAE - Regiões das ETAs"
+    ) %>% sf::st_zm()
+  ) %>%
+  addMarkers(
+    data = sf::st_read(
+      here("data", "cheias-iph.kmz"),
+      layer = "Saúde -- Hospitais, UBS, etc"
+    ) %>% sf::st_zm()
+  ) %>%
+  addMarkers(
+    data = sf::st_read(
+      here("data", "cheias-iph.kmz"),
+      layer = "Abrigos e Centro de Voluntários"
+    ) %>% sf::st_zm()
+  )
