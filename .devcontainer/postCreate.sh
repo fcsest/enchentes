3454ed9aa5e8509a82b75e57f386cadd3672fb1e
Fernando Souza <fcsest@gmail.com>
2024-05-06 21:50:32 +0000

    Amend devcontainer

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-echo 'Install R packages from lockfile...'
-# R -e 'renv::restore()'
-
 echo 'Install R packages for development...'
 R -e 'renv::install(c("languageserver", "httpgd", "attachment", "ManuelHentschel/vscDebugger", "dreamRs/prefixer"))'
